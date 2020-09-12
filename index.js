1fa1bf1b9ed4b75ff727b5c0585ada3bfca61d31
KonradOlech <frytas93@gmail.com>
2020-09-12 14:25:38 +0200

    adding algorithm comments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies} = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -31,4 +32,17 @@ World.add(world, walls);
 
 // Maze generation
 
-const grid = Array(3).fill(null).map(() => Array(3).fill(false));
+const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
