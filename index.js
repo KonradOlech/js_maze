cfc3126642ef3447295b8666de515e29ece93173
KonradOlech <48278875+KonradOlech@users.noreply.github.com>
2020-09-15 18:47:10 +0200

    Adding maze fall apart when game is over

--- index.js
+++ index.js
@@ -130,6 +130,7 @@ horizontals.forEach((row, rowIndex) => {
             unitLength,
             10,
             {
+                label: 'wall',
                 isStatic: true
             }
         );
@@ -150,6 +151,7 @@ verticals.forEach((row, rowIndex) => {
             10,
             unitLength,
             {
+                label: 'wall',
                 isStatic: true
             }
         );
@@ -212,7 +214,12 @@ Events.on(engine, 'collisionStart', event => {
         const labels = ['ball', 'goal'];
 
         if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
-            console.log('user won')
+            world.gravity.y = 1;
+            world.bodies.forEach(body => {
+                if (body.label === 'wall') {
+                    Body.setStatic(body, false);
+                }
+            })
         }
     })
 })
